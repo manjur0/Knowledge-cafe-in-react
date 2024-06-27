8c079936cf102e4a3ad1ab925a569b11059cb3b8
manjur0 <sfmanjur21@gmail.com>
2024-06-27 23:22:30 +0600

    post card designed

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,11 +1,46 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-  const { title } = blog;
+  const {
+    id,
+    title,
+    cover,
+    author_img,
+    author,
+    posted_date,
+    reading_time,
+    hashtags,
+  } = blog;
   return (
-    <>
-      <h2 className="text-4xl font-semibold">{title}</h2>
-    </>
+    <div className="mb-12">
+      <img
+        className="rounded-lg"
+        src={cover}
+        alt={`cover picture of the ${title}`}
+      />
+      <div className="flex justify-between items-center">
+        <div className="flex  gap-4 items-center my-6">
+          <img className="w-12" src={author_img} alt="" />
+          <div>
+            <h2>{author}</h2>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+
+      <h2 className="text-4xl font-semibold ">{title}</h2>
+      <p className="my-4">
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="">#{hash}</a>{" "}
+          </span>
+        ))}
+      </p>
+      <button className="text-blue-600 cursor-pointer underline">Mark as read</button>
+    </div>
   );
 };
 
